1a1102612ae9e1d1e06fbc308bbbbc6809eea971
john_1_1_1 <john_1_1_1@mail.ru>
2023-09-29 06:37:39 -0400

    Init Commit

---
+++ src/UchiRu.QuestBot.Worker/Program.cs
@@ -0,0 +1,10 @@
+using UchiRu.QuestBot.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
